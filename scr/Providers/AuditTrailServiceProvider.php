ce2dba8d8ea13e2de225deb748bf136a0b2c007d
Rollswan Acebedo <rollswan.acebedo@summitmedia.com.ph>
2021-09-02 23:20:17 +0800

    Update service provider

--- scr/Providers/AuditTrailServiceProvider.php
+++ scr/Providers/AuditTrailServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Rollswan\AuditTrail\Providers;
 
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\Router;
 use Rollswan\AuditTrail\Middleware\AuditTrailMiddleware;
@@ -9,32 +10,35 @@ use Rollswan\AuditTrail\Middleware\AuditTrailMiddleware;
 class AuditTrailServiceProvider extends ServiceProvider
 {
     /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot(Router $router)
     {
+        $source = realpath($raw = __DIR__ . '/../config/audit-trail.php') ?: $raw;
+
         $router->middlewareGroup('audit-trail', [AuditTrailMiddleware::class]);
 
-        // Make the configuration files publishable
-        $this->publishes(
-            [
-                __DIR__ . '/../config/audit-trail.php' => config_path('audit-trail.php')
-            ],
-            'config'
-        );
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            // Make the configuration files publishable
+            $this->publishes([$source => config_path('audit-trail.php')], 'audit-trail');
+        }
       
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (file_exists(config_path('audit-trail.php'))) {
+            $this->mergeConfigFrom(config_path('audit-trail.php'), 'audit-trail');
+        } else {
+            $this->mergeConfigFrom(__DIR__.'/config/audit-trail.php', 'audit-trail');
+        }
     }
 }
